199aa04bbc3dcfece28f2f05d7dc3dcd56d3f2b0
JiaG <giau32002@gmail.com>
2023-10-14 22:06:55 +0700

    add project

---
+++ src/main/java/com/openjfx/qllspahg/Main.java
@@ -0,0 +1,27 @@
+package com.openjfx.qllspahg;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import java.io.IOException;
+//import com.openjfx.qllspahg.database.Db;
+
+import static com.openjfx.qllspahg.database.Db.getConnection;
+
+public class Main extends Application {
+    @Override
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("fxml/DangNhap.fxml"));
+        Scene scene = new Scene(fxmlLoader.load());
+        stage.setTitle("Đăng nhập");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+
+        launch();
+
+    }
+}
