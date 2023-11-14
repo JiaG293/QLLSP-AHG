472956c8fa19a826e98409c7d7880b76b3bca405
JiaG <giau32002@gmail.com>
2023-11-14 21:24:43 +0700

    Update DanhSachHopDong.fxml 50%

--- src/main/java/com/openjfx/qllspahg/entity/HopDong.java
+++ src/main/java/com/openjfx/qllspahg/entity/HopDong.java
@@ -11,6 +11,25 @@ public class HopDong {
     private String email;
     private Date ngayKKHD;
     private Date ngayTLHD;
+    private boolean trangThaiHD;
+
+    public HopDong(String maHD, String tenKH, String sDT, String diaChi, String email, Date ngayKKHD, Date ngayTLHD, boolean trangThaiHD) {
+        this.maHD = maHD;
+        this.tenKH = tenKH;
+        this.sDT = sDT;
+        this.diaChi = diaChi;
+        this.email = email;
+        this.ngayKKHD = ngayKKHD;
+        this.ngayTLHD = ngayTLHD;
+        this.trangThaiHD = trangThaiHD;
+    }
+
+    public HopDong(String maHD) {
+        this.maHD = maHD;
+    }
+
+    public HopDong() {
+    }
 
     public HopDong(String maHD, String tenKH, String sDT, String diaChi, String email, Date ngayKKHD, Date ngayTLHD) {
         this.maHD = maHD;
@@ -20,13 +39,6 @@ public class HopDong {
         this.email = email;
         this.ngayKKHD = ngayKKHD;
         this.ngayTLHD = ngayTLHD;
-    }
-
-    public HopDong(String maHD) {
-        this.maHD = maHD;
-    }
-
-    public HopDong() {
     }
 
     public String getMaHD() {
@@ -85,6 +97,14 @@ public class HopDong {
         this.ngayTLHD = ngayTLHD;
     }
 
+    public boolean isTrangThaiHD() {
+        return trangThaiHD;
+    }
+
+    public void setTrangThaiHD(boolean trangThaiHD) {
+        this.trangThaiHD = trangThaiHD;
+    }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
@@ -96,5 +116,19 @@ public class HopDong {
     @Override
     public int hashCode() {
         return Objects.hash(maHD);
+    }
+
+    @Override
+    public String toString() {
+        return "HopDong {" +
+                "maHD='" + maHD + '\'' +
+                ", tenKH='" + tenKH + '\'' +
+                ", sDT='" + sDT + '\'' +
+                ", diaChi='" + diaChi + '\'' +
+                ", email='" + email + '\'' +
+                ", ngayKKHD=" + ngayKKHD +
+                ", ngayTLHD=" + ngayTLHD +
+                ", trangThaiHD=" + trangThaiHD +
+                '}' + "\n";
     }
 }
