c4363725925cdc3d576b91ab0f4a8906cff1e254
JiaG <giau32002@gmail.com>
2023-12-13 16:18:10 +0700

    Add screen quan ly tai khoan dang nhap, move DangNhap.fxml to DangNhap package

--- src/main/java/com/openjfx/qllspahg/Main.java
+++ src/main/java/com/openjfx/qllspahg/Main.java
@@ -12,7 +12,7 @@ public class Main extends Application {
     @Override
     public void start(Stage stage) throws IOException {
 
-        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("fxml/DangNhap.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("fxml/TaiKhoan/DangNhap.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 1200, 700);
 
         stage.centerOnScreen(); // Trung tam man hinh
---
+++ src/main/java/com/openjfx/qllspahg/dao/QuanLyTaiKhoanDaoImpl.java
@@ -0,0 +1,4 @@
+package com.openjfx.qllspahg.dao;
+
+public class QuanLyTaiKhoanDaoImpl {
+}
